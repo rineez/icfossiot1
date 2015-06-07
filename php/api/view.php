164954646563c754bd49ce89f6ecfb3a39cb5695
Rineez <rineez@gmail.com>
2015-06-07 15:09:29 +0530

    API end point to view data

---
+++ php/api/view.php
@@ -0,0 +1,5 @@
+<?php
+Read all contents of temperature log and put it in a variable $data
+$data = file_get_contents('data/store.txt');
+// Convert new lines(\n) to HTML <br> tag
+echo nl2br($data);
